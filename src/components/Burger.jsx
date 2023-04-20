64ccd0b199225e4d4199124032fcff6a81a6f517
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-04-20 14:56:37 -0300

    HEADER

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -18,8 +18,8 @@ export default function Sidebar() {
                     className="fixed  z-30 flex items-center cursor-pointer right-10 top-6"
                     fill="#2563EB"
                     viewBox="0 0 100 80"
-                    width="40"
-                    height="40"
+                    width="30"
+                    height="30"
                 >
                     <rect width="100" height="10"></rect>
                     <rect y="30" width="100" height="10"></rect>
